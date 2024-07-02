37c62e170d3828701d79efaaebbb3381fc33cd67
khoavpt <khoahd686@gmail.com>
2024-07-02 12:23:51 +0700

    fix mlp

--- model/mlp/mlp.py
+++ model/mlp/mlp.py
@@ -1,26 +1,26 @@
-import torch 
-import torch.nn as nn 
+import torch
+import torch.nn as nn
 from tqdm import tqdm
 
 class MLP(nn.Module):
-    def __init__(self, width, activation=nn.ReLU(), device='cpu'):
+    def __init__(self, width, activation_class=nn.ReLU, device='cpu'):
         super(MLP, self).__init__()
         self.width = width
         self.device = device
-        self.layer = []
+        self.layers = nn.ModuleList()
         for i in range(len(width) - 1):
-            one_layer = nn.Linear(width[i], width[i + 1], bias=True).to(device)
-            self.layer.append(one_layer)
+            linear_layer = nn.Linear(width[i], width[i + 1]).to(device)
+            nn.init.xavier_uniform_(linear_layer.weight)  # Xavier initialization
+            self.layers.append(linear_layer)
             if i != len(width) - 2:
-                self.layer.append(activation)
-        self.layer = nn.ModuleList(self.layer)
+                self.layers.append(activation_class().to(device))
 
     def forward(self, x):
-        for i in range(len(self.layer)):
-            x = self.layer[i](x)
-        return x
-    
-    def train_model(self, train_loader, val_loader, optimizer, loss_func, epochs=20):
+        for layer in self.layers:
+            x = layer(x)
+        return x.squeeze()
+
+    def train_model(self, train_loader, val_loader, optimizer, loss_func, epochs=20, is_lbfgs=False):
         train_loss_list = []
         val_loss_list = []
         pbar = tqdm(range(epochs))
@@ -28,20 +28,33 @@ class MLP(nn.Module):
         for epoch in pbar:
             self.train()
             train_loss = 0
-            val_loss = 0
             for x, y in train_loader:
                 x, y = x.to(self.device), y.to(self.device)
-                optimizer.zero_grad()
-                y_pred = self(x)
-                loss = loss_func(y_pred, y)
-                loss.backward()
-                optimizer.step()
+                    
+                def closure():
+                    optimizer.zero_grad()
+                    y_pred = self(x)
+                    loss = loss_func(y_pred, y)
+                    loss.backward()
+                    return loss
+                
+                if is_lbfgs:
+                    loss = optimizer.step(closure)
+                else:
+                    optimizer.zero_grad()
+                    y_pred = self(x)
+                    loss = loss_func(y_pred, y)
+                    loss.backward()
+                    optimizer.step()
+
                 train_loss += loss.item()
+                
             train_loss /= len(train_loader)
             train_loss_list.append(train_loss)
 
             self.eval()
-            with torch.no_grad():  
+            val_loss = 0
+            with torch.no_grad():
                 for x, y in val_loader:
                     x, y = x.to(self.device), y.to(self.device)
                     y_pred = self(x)
@@ -51,10 +64,8 @@ class MLP(nn.Module):
             val_loss_list.append(val_loss)
             pbar.set_description(f"train_loss: {train_loss}, val_loss: {val_loss}")
 
-            self.train()
-
         return train_loss_list, val_loss_list
-    
+
     def test_model(self, test_loader, loss_func):
         self.eval()
         test_loss = 0
@@ -65,4 +76,4 @@ class MLP(nn.Module):
                 loss = loss_func(y_pred, y)
                 test_loss += loss.item()
         test_loss /= len(test_loader)
-        return test_loss        
+        return test_loss
