9ccf68c0516d9991d70e240e5796986af1f52bc7
hoangvanphi2004 <hoangvanphi2004@gmail.com>
2024-08-19 21:55:11 +0700

    fix: data_gen

--- data1/gen_data.py
+++ data1/gen_data.py
@@ -1,12 +1,15 @@
-# tao data tu function
 import torch 
 
-def test_func(x, y):
-    return torch.exp(torch.sin(torch.pi * x) + y ** 2)
+def test_func(x):
+    return torch.exp(torch.sin(torch.pi * x[:, 0]) + x[:, 1] ** 2)
 
 def from_function_to_dataset(function, data_range = [-1, 1], data_dim = 1, num_of_data = 4000):
     """
         Remember: function always calculated with batch !!!
+        Args:
+            function: function we want to gen data
+            data_dim: number of variables in the input
+            num_of_data: amount of data we want to gen
     """    
     x = torch.rand(num_of_data, data_dim) * (data_range[1] - data_range[0]) + data_range[0]
     y = test_func(*x.T) + (torch.rand(1)[0] - 0.5) / 1e7
